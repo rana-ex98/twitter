d066b4caf8701b154b5f63ef5ac0b11e8c7e30f2
Sumedha Rana <sumirana5698@gmail.com>
2019-06-11 10:36:11 +0530

    lets extract tweets from twitter

---
+++ tweets.py
@@ -0,0 +1,19 @@
+
+import tweepy
+from tweepy import OAuthHandler
+
+
+access_token=""
+access_token_secret=""
+consumer_key=""
+consumer_secret=""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+        
+api = tweepy.API(auth)
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
+    
